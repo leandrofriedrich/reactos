05c5f8584553e9005dd3cc487e3e69d748343882
Justin Miller <justinmiller100@gmail.com>
2021-11-13 14:15:52 -0800

    [ARMLLB] Add the fundamental logic for identifying boards

--- boot/armllb/main.c
+++ boot/armllb/main.c
@@ -15,22 +15,22 @@ LlbStartup(IN ULONG Reserved,
 {
    /* Make sure we are booting on the correct kind of machine */
    if (BoardInfo != LlbHwGetBoardType()) while (TRUE);
-   
+
    /* Initialize hardware components */
    LlbHwInitialize();
-   
+
    /* Either QEMU or U-Boot itself should send this information */
    LlbEnvParseArguments(Arguments);
-   
+
    /* Clean up the screen */
    LlbVideoClearScreen(FALSE);
-   
+
    /* Print header */
    printf("\nReactOS ARM Low-Level Boot Loader [" __DATE__ " "__TIME__ "]\n");
-   
+
    /* Boot the OS Loader */
    LlbBoot();
    while (TRUE);
-   
+}
 
 /* EOF */
