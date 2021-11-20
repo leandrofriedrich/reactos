a8c3481998c36859d95ae63f78932b45afe1c963
Justin miller <justinmiller100@gmail.com>
2021-11-20 12:28:10 -0800

    [REACTOSUEFI] Create a basic EFI test environment

---
+++ boot/reactosuefi/core/include/uefigop.h
@@ -0,0 +1,111 @@
+/*
+ * PROJECT:     ReactOS UEFI Bootloader
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     UEFI GOP header
+ * COPYRIGHT:   Copyright 2021 Justin Miller <justinmiller100@gmail.com>
+ */
+
+#pragma once
+
+#define UNICODE
+
+// UEFI 2.9 Specs PDF Page 457
+#define EFI_BLACK                               0x00
+#define EFI_BLUE                                0x01
+#define EFI_GREEN                               0x02
+#define EFI_CYAN                                0x03
+#define EFI_RED                                 0x04
+#define EFI_MAGENTA                             0x05
+#define EFI_BROWN                               0x06
+#define EFI_LIGHTGRAY                           0x07
+#define EFI_DARKGRAY                            0x08
+#define EFI_LIGHTBLUE                           0x09
+#define EFI_LIGHTGREEN                          0x0A
+#define EFI_LIGHTCYAN                           0x0B
+#define EFI_LIGHTRED                            0x0C
+#define EFI_LIGHTMAGENTA                        0x0D
+#define EFI_YELLOW                              0x0E
+#define EFI_WHITE                               0x0F
+                                               
+#define EFI_BACKGROUND_BLACK                    0x00
+#define EFI_BACKGROUND_BLUE                     0x10
+#define EFI_BACKGROUND_GREEN                    0x20
+#define EFI_BACKGROUND_CYAN                     0x30
+#define EFI_BACKGROUND_RED                      0x40
+#define EFI_BACKGROUND_MAGENTA                  0x50
+#define EFI_BACKGROUND_BROWN                    0x60
+#define EFI_BACKGROUND_LIGHTGRAY                0x70
+
+
+// UEFI 2.9 Specs PDF Page 491
+typedef enum EFI_GRAPHICS_OUTPUT_BLT_OPERATION
+{
+    EfiBltVideoFill,
+    EfiBltVideoToBltBuffer,
+    EfiBltBufferToVideo,
+    EfiBltVideoToVideo,
+    EfiGraphicsOutputBltOperationMax
+} EFI_GRAPHICS_OUTPUT_BLT_OPERATION;
+
+// UEFI 2.9 Specs PDF Page 491
+typedef struct EFI_GRAPHICS_OUTPUT_BLT_PIXEL
+{
+    UINT8   Blue;
+    UINT8   Green;
+    UINT8   Red;
+    UINT8   Reserved;
+} EFI_GRAPHICS_OUTPUT_BLT_PIXEL;
+
+// UEFI 2.9 Specs PDF Page 486
+typedef enum EFI_GRAPHICS_PIXEL_FORMAT
+{
+    PixelRedGreenBlueReserved8BitPerColor,
+    PixelBlueGreenRedReserved8BitPerColor,
+    PixelBitMask,
+    PixelBltOnly,
+    PixelFormatMax
+} EFI_GRAPHICS_PIXEL_FORMAT;
+
+// UEFI 2.9 Specs PDF Page 485
+typedef struct EFI_PIXEL_BITMASK
+{
+    UINT32    RedMask;
+    UINT32    GreenMask;
+    UINT32    BlueMask;
+    UINT32    ReservedMask;
+} EFI_PIXEL_BITMASK;
+
+// UEFI 2.9 Specs PDF Page 486
+typedef struct EFI_GRAPHICS_OUTPUT_MODE_INFORMATION
+{
+    UINT32                      Version;
+    UINT32                      HorizontalResolution;
+    UINT32                      VerticalResolution;
+    EFI_GRAPHICS_PIXEL_FORMAT   PixelFormat;
+    EFI_PIXEL_BITMASK           PixelInformation;
+    UINT32                      PixelsPerScanLine;
+} EFI_GRAPHICS_OUTPUT_MODE_INFORMATION;
+
+// UEFI 2.9 Specs PDF Page 488
+typedef struct EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE
+{
+    UINT32                                MaxMode;
+    UINT32                                Mode;
+    EFI_GRAPHICS_OUTPUT_MODE_INFORMATION  *Info;
+    UINTN                                 SizeOfInfo;
+    EFI_PHYSICAL_ADDRESS                  FrameBufferBase;
+    UINTN                                 FrameBufferSize;
+} EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE;
+
+typedef EFI_STATUS (*EFI_GRAPHICS_OUTPUT_PROTOCOL_QUERY_MODE)(struct EFI_GRAPHICS_OUTPUT_PROTOCOL *This, UINT32 ModeNumber, UINTN *SizeOfInfo, EFI_GRAPHICS_OUTPUT_MODE_INFORMATION **Info);
+typedef EFI_STATUS (*EFI_GRAPHICS_OUTPUT_PROTOCOL_SET_MODE)(struct EFI_GRAPHICS_OUTPUT_PROTOCOL *This, UINT32 ModeNumber);
+typedef EFI_STATUS (*EFI_GRAPHICS_OUTPUT_PROTOCOL_BLT)(struct EFI_GRAPHICS_OUTPUT_PROTOCOL *This, EFI_GRAPHICS_OUTPUT_BLT_PIXEL *BltBuffer, EFI_GRAPHICS_OUTPUT_BLT_OPERATION BltOperation, UINTN SourceX, UINTN SourceY, UINTN DestinationX, UINTN DestinationY, UINTN Width, UINTN Height, UINTN Delta);
+
+// UEFI 2.9 Specs PDF Page 485
+typedef struct EFI_GRAPHICS_OUTPUT_PROTOCOL
+{
+    EFI_GRAPHICS_OUTPUT_PROTOCOL_QUERY_MODE  QueryMode;
+    EFI_GRAPHICS_OUTPUT_PROTOCOL_SET_MODE    SetMode;
+    EFI_GRAPHICS_OUTPUT_PROTOCOL_BLT         Blt;
+    EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE        *Mode;
+} EFI_GRAPHICS_OUTPUT_PROTOCOL;
